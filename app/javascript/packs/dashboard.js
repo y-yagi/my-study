0c1c4d9a88b45209fb8702e96fe0aaa978f52349
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-11-12 16:48:24 +0900

    regist `turbolinks:load` event

--- app/javascript/packs/dashboard.js
+++ app/javascript/packs/dashboard.js
@@ -10,5 +10,9 @@ const fetchSummary = () => {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  fetchSummary();
+  fetchSummary()
+})
+
+document.addEventListener('turbolinks:load', () => {
+  fetchSummary()
 })
