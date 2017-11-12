e4f7a3af129ed8870e96dc358498cb7db50c58a1
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-11-12 17:03:58 +0900

    get summary only need

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,12 @@
 class DashboardController < ApplicationController
   def show
-    hours = current_user.themes.eager_load(:studies).group('themes.title').sum('studies.hour')
-    minutes = current_user.themes.eager_load(:studies).group('themes.title').sum('studies.minute')
-    @summary = minutes.merge(hours) { |key, oldval, newval| oldval + newval * 60 }
+    respond_to do |format|
+      format.html
+      format.json do
+        hours = current_user.themes.eager_load(:studies).group('themes.title').sum('studies.hour')
+        minutes = current_user.themes.eager_load(:studies).group('themes.title').sum('studies.minute')
+        @summary = minutes.merge(hours) { |key, oldval, newval| oldval + newval * 60 }
+      end
+    end
   end
 end
