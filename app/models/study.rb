8eec3de26a139691b4790c353dd538a5f88ae905
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-11-12 17:13:58 +0900

    add validation for study time

--- app/models/study.rb
+++ app/models/study.rb
@@ -1,4 +1,12 @@
 class Study < ApplicationRecord
   belongs_to :user
   belongs_to :theme
+
+  validate :study_time
+
+  def study_time
+    if self.hour.zero? && self.minute.zero?
+      errors.add(:base, :hour_and_minute_blank)
+    end
+  end
 end
