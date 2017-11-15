f2ffd41c2280254e331af88d5abdeec59154b9b5
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-11-15 10:30:11 +0900

    correctly show nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def render_markdown(text)
     CommonMarker.render_html(text, :DEFAULT).html_safe
   end
+
+  def nav_class(path)
+    path == controller_name ? "active" : ""
+  end
 end
