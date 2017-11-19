1884e8f51ab658a7c551b4514e2dd712c728c3a7
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-11-19 14:00:28 +0900

    only load dashboard when chart exists

--- app/javascript/packs/dashboard.js
+++ app/javascript/packs/dashboard.js
@@ -2,10 +2,14 @@ import Chartist from "chartist"
 import "chartist/dist/chartist.min.css"
 
 const fetchSummary = () => {
+  if (!document.getElementById('ct-chart')) {
+    return
+  }
+
   fetch('/dashboard.json', { credentials: 'same-origin' }).then(response => {
     return response.json()
   }).then(data=> {
-    new Chartist.Bar('.ct-chart', data)
+    new Chartist.Bar('#ct-chart', data)
   })
 }
 
