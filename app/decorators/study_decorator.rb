75574f23cc0e0e717b4401b7e9400d25ed8ae5f8
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-12-25 06:56:56 +0900

    show first line in list

--- app/decorators/study_decorator.rb
+++ app/decorators/study_decorator.rb
@@ -2,4 +2,9 @@ module StudyDecorator
   def formatted_time
     "%d:%02d" % [hour, minute]
   end
+
+  def first_line_of_content
+    converted_content = CommonMarker.render_html(content, :DEFAULT)
+    converted_content.split("\n")[0].html_safe
+  end
 end
