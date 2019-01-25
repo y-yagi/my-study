e2d14b58f959a68dd5f7088c9b930581a32c1bf0
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2019-01-25 14:51:17 +0900

    add care about nil

--- app/models/study.rb
+++ app/models/study.rb
@@ -7,7 +7,7 @@ class Study < ApplicationRecord
   validate :study_time
 
   def study_time
-    if self.hour.zero? && self.minute.zero?
+    if self.hour&.zero? && self.minute&.zero?
       errors.add(:base, :hour_and_minute_blank)
     end
   end
